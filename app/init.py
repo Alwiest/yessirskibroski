56a2520c2e8113608af319094f36c20b94e5fc26
ФдAlwiest <user@example.com>
2025-12-12 16:04:56 +0300

    Базовая структура Фласка

---
+++ app/init.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev-key-123'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    return app
